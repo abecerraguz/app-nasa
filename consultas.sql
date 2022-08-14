87e88c3630d1890622ff614c2e2b7c0873ca762b
abecerraguz <alejandro.becerra@publimetro.cl>
2022-08-14 13:02:57 -0400

    Se guadan los datos para hacer Deploy

--- consultas.sql
+++ consultas.sql
@@ -1,3 +1,31 @@
+Host: ec2-3-225-110-188.compute-1.amazonaws.com
+Database:d8e23pirsgbvqj
+User:plhgziwympewbk
+Port:5432
+Password:d378cf61c2eab40529abc69ea7ae2dc24ff03f5eab300d43b19d5622acc0a56e
+URI:postgres://plhgziwympewbk:d378cf61c2eab40529abc69ea7ae2dc24ff03f5eab300d43b19d5622acc0a56e@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d8e23pirsgbvqj
+Heroku CLI
+heroku pg:psql postgresql-animate-40133 --app app-shopbikes
+
+-- Conectarnos Heroku
+psql -h ec2-3-225-110-188.compute-1.amazonaws.com -p 5432 -U plhgziwympewbk -W -d d8e23pirsgbvqj 
+
+-- IMPORTAR BASE DE DATOS A MI MAQUINA A POSTGRES HEROKU
+psql -h ec2-3-225-110-188.compute-1.amazonaws.com -d d8e23pirsgbvqj -U plhgziwympewbk -f /Users/abecerraguz/Desktop/db-postgres-bikeshop.sql;
+
+/*
+DEPLOY A HEROKU
+
+heroku git:remote -a app-shopbikes
+git push heroku master
+
+// Para abrir
+heroku open
+
+// Para ver el proceso
+heroku logs --tail
+*/
+
 select st.store_name, p.product_id, p.product_name , s.quantity from categories c 
 inner join products p on (p.category_id = c.category_id) 
 inner join stocks s on (s.product_id = p.product_id) 
