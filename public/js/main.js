c1b3eb50c19518a7b319b4d9655a696c561562fe
abecerraguz <alejandro.becerra@publimetro.cl>
2022-08-15 11:42:52 -0400

    Se guarda cambio

--- public/js/main.js
+++ public/js/main.js
@@ -24,7 +24,7 @@ const pintarTable = async()=>{
         .then( result => {
             const info = result.data
             info.forEach(element =>{
-                const str = element.store_name
+                const str = element.product_name
                 const replaced = str.replace(/'/g, ``);
                 infoTable.innerHTML+=`
                 <tr>
@@ -70,7 +70,7 @@ enviar.addEventListener('click', async(e)=>{
         if(info.length != 0){
             infoTable.innerHTML=``
             info.forEach(element =>{
-                const str = element.store_name
+                const str = element.product_name
                 const replaced = str.replace(/'/g, ``);
                 infoTable.innerHTML+=`
                 <tr>
