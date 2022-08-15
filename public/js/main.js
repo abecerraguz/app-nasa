d973cc5457fbb579dc1fd49996c1aec84fa36949
abecerraguz <alejandro.becerra@publimetro.cl>
2022-08-15 11:28:50 -0400

    Se guardan los cambios

--- public/js/main.js
+++ public/js/main.js
@@ -24,6 +24,8 @@ const pintarTable = async()=>{
         .then( result => {
             const info = result.data
             info.forEach(element =>{
+                const str = element.store_name
+                const replaced = str.replace(/'/g, ``);
                 infoTable.innerHTML+=`
                 <tr>
                 <td>${element.store_name}</td>
@@ -31,7 +33,7 @@ const pintarTable = async()=>{
                 <td>${element.product_name}</td>
                 <td>${element.quantity}</td>
                 <td>
-                <button type="button" class="btn btn-danger btn-sm" id="${element.product_id}" onclick="infoProducto('${element.store_name}','${element.product_id}','${element.product_name}','${element.quantity}')">
+                <button type="button" class="btn btn-danger btn-sm" id="${element.product_id}" onclick="infoProducto('${element.store_name}','${element.product_id}','${replaced}','${element.quantity}')">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-in" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
                     <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"/>
@@ -68,7 +70,7 @@ enviar.addEventListener('click', async(e)=>{
         if(info.length != 0){
             infoTable.innerHTML=``
             info.forEach(element =>{
-                
+
                 infoTable.innerHTML+=`
                 <tr>
                 <td>${element.store_name}</td>
