8de6ca9283dc3d17db958f5a160e1024d4735de5
abecerraguz <alejandro.becerra@publimetro.cl>
2022-08-16 21:36:24 -0400

    Se guardan los cambios

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,61 @@
 import * as UI from './components/interfaz.js';
-import pintarTable  from './components/pintarTable.js';
-import replaced  from './components/replaced.js';
-import infoProducto from './components/infoProducto.js';
+// import pintarTable  from './components/pintarTable.js';
+// import replaced  from './components/replaced.js';
+// import infoProducto from './components/infoProducto.js';
 UI.loading.style.display = "none";
+
+const infoProducto = ( store_name, product_id, product_name, quantity)=>{
+    $('#mensajesModal').modal('toggle');
+    UI.modalBody.innerHTML=``
+    UI.modalBody.innerHTML=`<p>En la tienda <span class="text-uppercase text-danger">${store_name}</span> el producto <span class="text-uppercase text-danger">${product_name}</span> tiene un stock de <span class="text-uppercase text-danger">${quantity} bicicletas</span></p>`;
+    UI.infoTienda.innerHTML=`<h5 class="my-0">Tienda ${store_name}</h5>`      
+}
+
+const replaced = (str)=>{
+    const out = str
+    if(/'/g.test(str)){
+        const replaced = str.replace(/'/g,`&apos;`);
+        return replaced
+    }
+
+    if(/"/g.test(str)){
+        const replaced = str.replace(/"/g ,`&quot;`);
+        return replaced
+    }
+    return out
+}
+
+const pintarTable = async()=>{
+    UI.loading.style.display = "flex";
+    await axios.get('https://app-shopbikes.herokuapp.com/ordenes')
+        .then( result => {
+            const info = result.data
+            info.forEach(element =>{
+                UI.infoTable.innerHTML+=`
+                <tr>
+                <td>${element.store_name}</td>
+                <td>${element.product_id}</td>
+                <td>${replaced(element.product_name)}</td>
+                <td>${element.quantity}</td>
+                <td>
+                <button type="button" class="btn btn-danger btn-sm" id="${element.product_id}" onclick='infoProducto("${element.store_name}","${element.product_id}","${replaced(element.product_name)}","${element.quantity}") '>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-in" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                    <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"/>
+                    <path fill-rule="evenodd" d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z"/>
+                    </svg>
+                </button>
+                </td>
+            </tr>`
+            })
+            UI.loading.style.display = "none";
+        })    
+}
 
 // Filtrar busqueda
 UI.enviar.addEventListener('click', async(e)=>{
     e.preventDefault()
-    // const category_id = document.querySelector('#category_id').value
-    // const store_id = document.querySelector('#store_id').value
-    // const brand_name = document.querySelector('#brand_name').value
-    // const infoTable = document.querySelector('#infoTable')
-
+ 
     UI.loading.style.display = "flex";
 
     await axios.post('https://app-shopbikes.herokuapp.com/ordenes',{
