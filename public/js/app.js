5b3008cc603eb4ec491389a5c624a9c844a99bcf
abecerraguz <alejandro.becerra@publimetro.cl>
2022-08-17 22:05:09 -0400

    Se guardan los cambios

--- public/js/app.js
+++ public/js/app.js
@@ -43,11 +43,18 @@ const pintarTable = async()=>{
                 <td data-label="Inventario">${element.quantity}</td>
                 <td>
                 <button type="button" class="btn btn-danger btn-sm" id="${element.product_id}" onclick='infoProducto("${element.store_name}","${element.product_id}","${replaced(element.product_name)}","${element.quantity}")'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-in" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                    <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"/>
-                    <path fill-rule="evenodd" d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z"/>
-                    </svg>
+                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox="0 0 576 512" style="enable-background:new 0 0 576 512;" xml:space="preserve">
+           <style type="text/css">
+               .st0{fill:#FFFFFF;}
+           </style>
+           <path class="st0" d="M279.6,160.4c2.8-0.3,5.6-0.4,8.4-0.4c53,0,96,42.1,96,96c0,53-43,96-96,96c-53.9,0-96-43-96-96
+               c0-2.8,0.1-5.6,0.4-8.4c9.3,4.5,20.1,8.4,31.6,8.4c35.3,0,64-28.7,64-64C288,180.5,284.1,169.7,279.6,160.4z M480.6,112.6
+               c46.8,43.4,78.1,94.5,92.9,131.1c3.3,7.9,3.3,16.7,0,24.6c-14.8,35.7-46.1,86.8-92.9,131.1C433.5,443.2,368.8,480,288,480
+               s-145.5-36.8-192.6-80.6c-46.8-44.3-78.1-95.4-93-131.1c-3.3-7.9-3.3-16.7,0-24.6c14.9-36.6,46.2-87.7,93-131.1
+               C142.5,68.8,207.2,32,288,32S433.5,68.8,480.6,112.6L480.6,112.6z M288,112c-79.5,0-144,64.5-144,144s64.5,144,144,144
+               s144-64.5,144-144S367.5,112,288,112z"/>
+           </svg>
                 </button>
                 </td>
             </tr>`
