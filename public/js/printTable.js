f9d1c1587ccf956e834ced4f6044126551b1a5a6
abecerraguz <alejandro.becerra@publimetro.cl>
2022-08-19 06:45:30 -0400

    Se guardan los cambios

--- public/js/printTable.js
+++ public/js/printTable.js
@@ -5,30 +5,26 @@ import replaceQuotes from './replaceQuotes.js';
 const pintarTable = async ( brand_name, category_id, store_id ) => {
     console.log( 'Llego info-->', brand_name,category_id,store_id )
     UI.loading.style.display = "flex";
-    await axios.post('https://app-shopbikes.herokuapp.com/ordenes',{
-        category_id,
-        store_id,
-        brand_name
-    })
+    await axios.get('https://app-shopbikes.herokuapp.com/ordenes')
     // await axios.get('http://localhost:3000/ordenes')
         .then( result => {
             const info = result.data
             console.log('Salida de info',info)
-            // info.forEach(element =>{
-            //     UI.infoTable.innerHTML+=`
-            //     <tr>
-            //     <td data-label="Tienda">${element.store_name}</td>
-            //     <td data-label="ID">${element.product_id}</td>
-            //     <td data-label="Producto">${replaceQuotes(element.product_name)}</td>
-            //     <td data-label="Inventario">${element.quantity}</td>
-            //     <td>
-            //         <button type="button" class="btn btn-danger btn-sm" id="${element.product_id}">
-            //             Ver información
-            //         </button>
-            //     </td>
-            // </tr>`
+            info.forEach(element =>{
+                UI.infoTable.innerHTML+=`
+                <tr>
+                <td data-label="Tienda">${element.store_name}</td>
+                <td data-label="ID">${element.product_id}</td>
+                <td data-label="Producto">${replaceQuotes(element.product_name)}</td>
+                <td data-label="Inventario">${element.quantity}</td>
+                <td>
+                    <button type="button" class="btn btn-danger btn-sm" id="${element.product_id}">
+                        Ver información
+                    </button>
+                </td>
+            </tr>`
 
-            // })
+            })
 
             UI.loading.style.display = "none";
             const infoButton = document.querySelectorAll('#infoTable tr td button');
