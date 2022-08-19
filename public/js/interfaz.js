641b54ba544a762df18bef847bb395b3800b1d82
abecerraguz <alejandro.becerra@publimetro.cl>
2022-08-18 22:20:46 -0400

    Se guardan los cambios

--- public/js/interfaz.js
+++ public/js/interfaz.js
@@ -1,8 +1,7 @@
-const enviar = document.querySelector('#enviar'),
+export const enviar = document.querySelector('#enviar'),
 reset = document.querySelector('#reset'),
 loading = document.querySelector('.contentSpinnerLoading'),
 infoTable = document.querySelector('#infoTable'),
 category_id = document.querySelector('#category_id'),
 store_id = document.querySelector('#store_id'),
 brand_name = document.querySelector('#brand_name');
-export default enviar;
