3f5d414f5cdc961a596e7c8d775f35436dce75f3
abecerraguz <alejandro.becerra@publimetro.cl>
2022-08-19 07:42:49 -0400

    Se guardan los cambios

--- public/js/app.js
+++ public/js/app.js
@@ -48,50 +48,51 @@ UI.enviar.addEventListener('click', async(e)=>{
     const brand_name = document.querySelector('#brand_name').value
     const category_id = document.querySelector('#category_id').value
     const store_id = document.querySelector('#store_id').value
-    await axios.post('https://app-shopbikes.herokuapp.com/ordenes',{
-    //await axios.post('http://localhost:3000/ordenes',{
-        category_id,
-        store_id,
-        brand_name
-    }).then( result =>{
-        const info = result.data
-        if(info.length != 0){
-            UI.infoTable.innerHTML=``
-            info.forEach(element =>{
-                UI.infoTable.innerHTML+=`
-                <tr>
-                <td data-label="Tienda">${element.store_name}</td>
-                <td data-label="ID">${element.product_id}</td>
-                <td data-label="Producto">${replaceQuotes(element.product_name)}</td>
-                <td data-label="Inventario">${element.quantity}</td>
-                <td>
-                    <button type="button" class="btn btn-danger btn-sm" id="${element.product_id}">
-                        Ver información
-                    </button>
-                </td>
-            </tr>`
-            })
-            UI.loading.style.display = "none";
-            const infoButton = document.querySelectorAll('#infoTable tr td button');
-            infoButton.forEach( element => {
-                element.addEventListener('click',(e)=>{
-                    e.preventDefault();
-                    const infoId = e.target.id
-                    const buscar = info.find( element => element.product_id == infoId )
-                    infoProducto(buscar.store_name, buscar.product_id, replaceQuotes(buscar.product_name), buscar.quantity)
-                })
-            })
+    printTable( category_id, store_id, brand_name )
+    // await axios.post('https://app-shopbikes.herokuapp.com/ordenes',{
+    // //await axios.post('http://localhost:3000/ordenes',{
+    //     category_id,
+    //     store_id,
+    //     brand_name
+    // }).then( result =>{
+    //     const info = result.data
+    //     if(info.length != 0){
+    //         UI.infoTable.innerHTML=``
+    //         info.forEach(element =>{
+    //             UI.infoTable.innerHTML+=`
+    //             <tr>
+    //             <td data-label="Tienda">${element.store_name}</td>
+    //             <td data-label="ID">${element.product_id}</td>
+    //             <td data-label="Producto">${replaceQuotes(element.product_name)}</td>
+    //             <td data-label="Inventario">${element.quantity}</td>
+    //             <td>
+    //                 <button type="button" class="btn btn-danger btn-sm" id="${element.product_id}">
+    //                     Ver información
+    //                 </button>
+    //             </td>
+    //         </tr>`
+    //         })
+    //         UI.loading.style.display = "none";
+    //         const infoButton = document.querySelectorAll('#infoTable tr td button');
+    //         infoButton.forEach( element => {
+    //             element.addEventListener('click',(e)=>{
+    //                 e.preventDefault();
+    //                 const infoId = e.target.id
+    //                 const buscar = info.find( element => element.product_id == infoId )
+    //                 infoProducto(buscar.store_name, buscar.product_id, replaceQuotes(buscar.product_name), buscar.quantity)
+    //             })
+    //         })
 
-        }else{
-            UI.loading.style.display = "none";
-            UI.infoTable.innerHTML=`<tr> 
-                <td colspan="5">
-                    <h5 class="text-center my-5">Lo sentimos la orden que esta buscando no existe!!</h5>
-                </td>
-            </tr>`
-        }
+    //     }else{
+    //         UI.loading.style.display = "none";
+    //         UI.infoTable.innerHTML=`<tr> 
+    //             <td colspan="5">
+    //                 <h5 class="text-center my-5">Lo sentimos la orden que esta buscando no existe!!</h5>
+    //             </td>
+    //         </tr>`
+    //     }
         
-    })
+   
 })
 
 
--- public/js/printTable.js
+++ public/js/printTable.js
@@ -13,32 +13,41 @@ const printTable = async ( category_id, store_id, brand_name ) => {
     // await axios.get('http://localhost:3000/ordenes')
         .then( result => {
             const info = result.data
-            info.forEach(element =>{
-                UI.infoTable.innerHTML+=`
-                <tr>
-                <td data-label="Tienda">${element.store_name}</td>
-                <td data-label="ID">${element.product_id}</td>
-                <td data-label="Producto">${replaceQuotes(element.product_name)}</td>
-                <td data-label="Inventario">${element.quantity}</td>
-                <td>
-                    <button type="button" class="btn btn-danger btn-sm" id="${element.product_id}">
-                        Ver información
-                    </button>
-                </td>
-            </tr>`
-
-            })
-
-            UI.loading.style.display = "none";
-            const infoButton = document.querySelectorAll('#infoTable tr td button');
-            infoButton.forEach( element => {
-                element.addEventListener('click',(e)=>{
-                    e.preventDefault();
-                    const infoId = e.target.id
-                    const buscar = info.find( element => element.product_id == infoId )
-                    infoProducto(buscar.store_name, buscar.product_id, replaceQuotes(buscar.product_name), buscar.quantity)
+            if(info.length != 0){
+                UI.infoTable.innerHTML=``
+                info.forEach(element =>{
+                    UI.infoTable.innerHTML+=`
+                    <tr>
+                    <td data-label="Tienda">${element.store_name}</td>
+                    <td data-label="ID">${element.product_id}</td>
+                    <td data-label="Producto">${replaceQuotes(element.product_name)}</td>
+                    <td data-label="Inventario">${element.quantity}</td>
+                    <td>
+                        <button type="button" class="btn btn-danger btn-sm" id="${element.product_id}">
+                            Ver información
+                        </button>
+                    </td>
+                </tr>`
                 })
-            })
+                UI.loading.style.display = "none";
+                const infoButton = document.querySelectorAll('#infoTable tr td button');
+                infoButton.forEach( element => {
+                    element.addEventListener('click',(e)=>{
+                        e.preventDefault();
+                        const infoId = e.target.id
+                        const buscar = info.find( element => element.product_id == infoId )
+                        infoProducto(buscar.store_name, buscar.product_id, replaceQuotes(buscar.product_name), buscar.quantity)
+                    })
+                })
+    
+            }else{
+                UI.loading.style.display = "none";
+                UI.infoTable.innerHTML=`<tr> 
+                    <td colspan="5">
+                        <h5 class="text-center my-5">Lo sentimos la orden que esta buscando no existe!!</h5>
+                    </td>
+                </tr>`
+            }
 
         })    
 }
